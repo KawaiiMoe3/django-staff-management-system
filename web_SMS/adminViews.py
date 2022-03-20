355bb97fa18b7bf9348d11d9d8a02d843fb8b6ad
KawaiiMoe3 <tanjunsam3@gmail.com>
2022-03-20 18:37:58 +0800

    change form to django form

--- web_SMS/adminViews.py
+++ web_SMS/adminViews.py
@@ -2,6 +2,7 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib import messages
+from web_SMS.form import AddStaffForm, EditStaffForm
 from web_SMS.models import CustomUser, Staffs
 from django.core.files.storage import FileSystemStorage
 
@@ -11,37 +12,52 @@ def Index(request):
 
 #Page addStaff
 def addStaff(request):
-    return render(request, 'smsys_admin/addStaff.html')
+    #Get the AddStaffForm() model
+    form = AddStaffForm()
+    context = {
+        'form' : form
+    }
+    return render(request, 'smsys_admin/addStaff.html', context)
 
 #Do add staff
 def doAddStaff(request):
     if request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        email = request.POST.get('email')
-        gender = request.POST.get('gender')
-        telno = request.POST.get('telno')
-        address = request.POST.get('address')
+        #Get the AddStaffForm() model
+        form = AddStaffForm(request.POST, request.FILES)
+        #Check whether user's input is valid or not
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            email = form.cleaned_data['email']
+            gender = form.cleaned_data['gender']
+            telno = form.cleaned_data['telno']
+            address = form.cleaned_data['address']
 
-        profile_pic = request.FILES['profile_pic']
-        # create a new instance of FileSystemStorage
-        fs = FileSystemStorage()
-        filename = fs.save(profile_pic.name, profile_pic)
-        # the fileurl variable now contains the url to the file. This can be used to serve the file when needed.
-        profile_pic_url = fs.url(filename)
+            profile_pic = request.FILES['profile_pic']
+            # create a new instance of FileSystemStorage
+            fs = FileSystemStorage()
+            filename = fs.save(profile_pic.name, profile_pic)
+            # the fileurl variable now contains the url to the file. This can be used to serve the file when needed.
+            profile_pic_url = fs.url(filename)
 
-        try:
-            user = CustomUser.objects.create_user(username=username, password=password, email=email, user_type=2)
-            user.staffs.gender = gender
-            user.staffs.telno = telno
-            user.staffs.address = address
-            user.staffs.profile_pic = profile_pic_url
-            user.save()
-            messages.success(request, "Staff added successfully!")
-            return redirect('add_staff')
-        except:
-            messages.error(request, "Staff added failure...Please try again.")
-            return redirect('add_staff')
+            try:
+                user = CustomUser.objects.create_user(username=username, password=password, email=email, user_type=2)
+                user.staffs.gender = gender
+                user.staffs.telno = telno
+                user.staffs.address = address
+                user.staffs.profile_pic = profile_pic_url
+                user.save()
+                messages.success(request, "Staff added successfully!")
+                return redirect('add_staff')
+            except:
+                messages.error(request, "Staff added failure...Please try again.")
+                return redirect('add_staff')
+        else:
+            form = AddStaffForm(request.POST)
+            context = {
+                'form' : form
+            }
+            return render(request, 'smsys_admin/addStaff.html', context)
     else:
         return HttpResponse("Method not allowed")
 
@@ -57,54 +73,84 @@ def listStaff(request):
 
 #Page Edit staff
 def editStaff(request, staff_id):
+    #Add ID into session
+    #Store the staff_id into Session in Backend when admin edit staff before showing the data into form
+    request.session['staff_id'] = staff_id
     staff = Staffs.objects.get(admin=staff_id)
+    form = EditStaffForm()
+    form.fields['username'].initial = staff.admin.username
+    form.fields['email'].initial = staff.admin.email
+    form.fields['gender'].initial = staff.gender
+    form.fields['telno'].initial = staff.telno
+    form.fields['address'].initial = staff.address
+    form.fields['profile_pic'].initial = staff.profile_pic
     context = {
-        'staff' : staff,
+        'form' : form,
         'id' : staff_id,
+        'username' : staff.admin.username,
     }
     return render(request, 'smsys_admin/editStaff.html', context)
 
 #Do edit staff
 def doEditStaff(request):
     if request.method == "POST":
-        staff_id = request.POST.get("staff_id")
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        gender = request.POST.get('gender')
-        telno = request.POST.get('telno')
-        address = request.POST.get('address')
+        #staff_id come from which stored in the session
+        staff_id = request.session.get("staff_id")
+        #Check whether staff_id is exists or not
+        if staff_id == None:
+            return redirect('list_staff')
+        
+        #Get the EditStaffForm() model
+        form = EditStaffForm(request.POST, request.FILES)
+        #Check whether user's input is valid or not
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            gender = form.cleaned_data['gender']
+            telno = form.cleaned_data['telno']
+            address = form.cleaned_data['address']
 
-        #Save a new picture if file is selected, else will not update old picture
-        if request.FILES.get('profile_pic', False):
-            profile_pic = request.FILES['profile_pic']
-            # create a new instance of FileSystemStorage
-            fs = FileSystemStorage()
-            filename = fs.save(profile_pic.name, profile_pic)
-            # the fileurl variable now contains the url to the file. This can be used to serve the file when needed.
-            profile_pic_url = fs.url(filename)
+            #Save a new picture if file is selected, else will not update old picture
+            if request.FILES.get('profile_pic', False):
+                profile_pic = request.FILES['profile_pic']
+                # create a new instance of FileSystemStorage
+                fs = FileSystemStorage()
+                filename = fs.save(profile_pic.name, profile_pic)
+                # the fileurl variable now contains the url to the file. This can be used to serve the file when needed.
+                profile_pic_url = fs.url(filename)
+            else:
+                profile_pic_url = None
+
+            try:
+                user = CustomUser.objects.get(id=staff_id)
+                user.username = username
+                user.email = email
+                user.save()
+
+                staff_model = Staffs.objects.get(admin=staff_id)
+                staff_model.gender = gender
+                staff_model.telno = telno
+                staff_model.address = address
+                #If file is selected, save a new picture
+                if profile_pic_url != None:
+                    staff_model.profile_pic = profile_pic_url
+                staff_model.save()
+                #Deleting the staff_id from session after staff edited
+                del request.session['staff_id']
+                messages.success(request, "Staff edited successfully!")
+                return redirect('edit_staff/'+staff_id)
+            except:
+                messages.error(request, "Staff edited failure...Please try again.")
+                return redirect('edit_staff/'+staff_id)
         else:
-            profile_pic_url = None
-
-        try:
-            user = CustomUser.objects.get(id=staff_id)
-            user.username = username
-            user.email = email
-            user.save()
-
-            staff_model = Staffs.objects.get(admin=staff_id)
-            staff_model.gender = gender
-            staff_model.telno = telno
-            staff_model.address = address
-            #If file is selected, save a new picture
-            if profile_pic_url != None:
-                staff_model.profile_pic = profile_pic_url
-            staff_model.save()
-
-            messages.success(request, "Staff edited successfully!")
-            return redirect('edit_staff/'+staff_id)
-        except:
-            messages.error(request, "Staff edited failure...Please try again.")
-            return redirect('edit_staff/'+staff_id)
+            form = EditStaffForm(request.POST)
+            staff = Staffs.objects.get(admin=staff_id)
+            context = {
+                'form' : form,
+                'id' : staff_id,
+                'username' : staff.admin.username,
+            }
+            return render(request, 'smsys_admin/editStaff.html', context)
     else:
         return HttpResponse("Method not allowed!")
 
--- web_SMS/form.py
+++ web_SMS/form.py
@@ -1,11 +1,28 @@
 from django import forms
 
-#Create addStaffForm
-class addStaffForm(forms.Form):
-    username = forms.CharField(label="Username", max_length=50)
-    password = forms.CharField(label="Password", max_length=50)
-    email = forms.CharField(label="Email", max_length=50)
-    gender = forms.CharField(label="Gender", max_length=50)
-    telno = forms.CharField(label="Telno", max_length=50)
-    address = forms.CharField(label="Address", max_length=50)
-    profile_pic = forms.CharField(label="Profile Image", max_length=50)
+#Create AddStaffForm for addStaff.html
+class AddStaffForm(forms.Form):
+    username = forms.CharField(label="Username", max_length=50, widget=forms.TextInput(attrs={"class":"form-control", "placeholder":"Enter username"}))
+    password = forms.CharField(label="Password", max_length=50, widget=forms.PasswordInput(attrs={"class":"form-control", "placeholder":"Enter password"}))
+    email = forms.EmailField(label="Email", widget=forms.EmailInput(attrs={"class":"form-control", "placeholder":"Enter email"}))
+    gender_choice = [
+        ('Male','Male'),
+        ('Female','Female')
+    ]
+    gender = forms.CharField(label='Gender', widget=forms.RadioSelect(choices=gender_choice))
+    telno = forms.RegexField(label="Telno", regex='^(\+?6?01)[02-46-9]-*[0-9]{7}$|^(\+?6?01)[1]-*[0-9]{8}$', widget=forms.TextInput(attrs={"class":"form-control", "placeholder":"Enter telno, exp:+60,60,0123456789"}))
+    address = forms.CharField(label="Address", max_length=100,widget=forms.TextInput(attrs={"class":"form-control","placeholder":"Enter address"}))
+    profile_pic = forms.FileField(label="Profile Image", max_length=50,widget=forms.FileInput(attrs={"class":"form-control"}))
+
+#Create EditStaffForm for editStaff.html
+class EditStaffForm(forms.Form):
+    username = forms.CharField(label="Username", max_length=50, widget=forms.TextInput(attrs={"class":"form-control", "placeholder":"Enter username"}))
+    email = forms.EmailField(label="Email", widget=forms.EmailInput(attrs={"class":"form-control", "placeholder":"Enter email"}))
+    gender_choice = [
+        ('Male','Male'),
+        ('Female','Female')
+    ]
+    gender = forms.CharField(label='Gender', widget=forms.RadioSelect(choices=gender_choice))
+    telno = forms.RegexField(label="Telno", regex='^(\+?6?01)[02-46-9]-*[0-9]{7}$|^(\+?6?01)[1]-*[0-9]{8}$', widget=forms.TextInput(attrs={"class":"form-control", "placeholder":"Enter telno, exp:+60,60,0123456789"}))
+    address = forms.CharField(label="Address", max_length=100,widget=forms.TextInput(attrs={"class":"form-control","placeholder":"Enter address"}))
+    profile_pic = forms.FileField(label="Profile Image", max_length=50,widget=forms.FileInput(attrs={"class":"form-control"}), required=False)
